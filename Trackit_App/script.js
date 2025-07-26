df603aae2918112001b65331dcad3bba143551fb
Tanmay <tanmaykangule7271@gmail.com>
2025-07-26 19:23:53 +0530

    Added expense entry feature with date, category, and amount display

--- Trackit_App/script.js
+++ Trackit_App/script.js
@@ -32,3 +32,40 @@ document.addEventListener('DOMContentLoaded', function () {
         updateIcons();
     });
 });
+
+// <----------------Fetching expense list from the form filled by user and adding to the UI--------------------->
+const expenseList = document.getElementById('expenseList');
+const descriptionInput = document.querySelector('#description');
+const amountInput = document.querySelector('#amount');
+const categoryInput = document.querySelector('#category');
+const dateInput = document.querySelector('#date');
+const addButton = document.querySelector('#addBtn');
+let expenses = []; // To store expenses.
+
+function addExpense() {
+    if (!dateInput.value || !categoryInput.value || !amountInput.value) {
+        alert("Please fill out all fields");
+        return;
+    }
+    const expense = {
+        date: dateInput.value,
+        category: categoryInput.value,
+        amount: amountInput.value
+    };
+    expenses.push(expense);
+
+    const li = document.createElement('li');
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('removeBtn');
+    li.classList.add('expense-item');
+    li.innerHTML = `<span class="text-left">${expense.date} | ${expense.category}</span>
+                    <span class="text-right">${expense.amount}</span>`
+    expenseList.appendChild(li);
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener('click', function () {
+        expenseList.removeChild(li);
+    });
+    li.appendChild(removeButton);
+    document.querySelector('.containerForm').reset();
+}
+addButton.addEventListener('click', addExpense);
